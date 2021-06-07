cf607e8643477424b84a6fa26795364a3ade625f
nicolasme <xx.nicolas.mertens.xx@gmail.com>
2021-06-07 11:14:03 +0200

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/nicolasme/s7comm
 
 go 1.16
 
-require github.com/influxdata/telegraf v1.18.3 // indirect
+require (
+	github.com/influxdata/telegraf v1.18.3
+	github.com/robinson/gos7 v0.0.0-20201216125248-2dd72fe148d3 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+)
--- plugins/inputs/s7comm/s7comm.go
+++ plugins/inputs/s7comm/s7comm.go
@@ -1,15 +1,20 @@
 package s7comm
 
 import (
+	"time"
+
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/robinson/gos7"
 )
 
 // S7Comm
 type S7Comm struct {
 	MetricName string `toml:"name"`
-	Endpoint   string `toml:"endpoint"`
-	Rack       string `toml:"security_policy"`
-	Slot       string `toml:"security_mode"`
+	Endpoint   string `toml:"plc_ip"`
+	Rack       int    `toml:"plc_rack"`
+	Slot       int    `toml:"plc_slot"`
 
 	Timeout     config.Duration `toml:"connect_timeout"`
 	IdleTimeout config.Duration `toml:"request_timeout"`
@@ -18,10 +23,47 @@ type S7Comm struct {
 	// Groups    []GroupSettings `toml:"group"`
 
 	// // internal values
-	// client *TCPClientHandler
+	handler *gos7.TCPClientHandler
+	client  *gos7.Client
 	// req    *ua.ReadRequest
 	// opts   []opcua.Option
 }
 
+func (s7Client *S7Comm) Connect() error {
+	s7Client.handler = gos7.NewTCPClientHandler(s7Client.Endpoint, s7Client.Rack, s7Client.Slot)
+	s7Client.handler.Timeout = time.Duration(s7Client.Timeout)
+	s7Client.handler.IdleTimeout = time.Duration(s7Client.IdleTimeout)
+
+	err := s7Client.handler.Connect()
+	if err != nil {
+		return err
+	}
+
+	defer s7Client.handler.Close()
+
+	return nil
+}
+
+func (s7Client *S7Comm) Read() {
+
+}
+
+func (s7Client *S7Comm) Stop() error {
+	err := s7Client.handler.Close()
+
+	return err
+}
+
+// Add this plugin to telegraf
 func init() {
+	inputs.Add("s7comm", func() telegraf.Input {
+		return &s7Client{
+			MetricName:  "s7comm",
+			Endpoint:    "opc.tcp://localhost:4840",
+			Rack:        "0",
+			Slot:        "2",
+			Timeout:     config.Duration(5 * time.Second),
+			IdleTimeout: config.Duration(10 * time.Second),
+		}
+	})
 }
