c6ff80308145586c2da3bba57c67fddf210ea8a3
nicolasme <xx.nicolas.mertens.xx@gmail.com>
2021-06-07 08:56:51 +0200

    init

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	// TODO: import your plugins
-	// _ "github.com/my_github_user/my_plugin_repo/plugins/inputs/mypluginname"
+	_ "github.com/nicolasme/s7comm/plugins/inputs/s7comm"
 
 	"github.com/influxdata/telegraf/plugins/common/shim"
 )
